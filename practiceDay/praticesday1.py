6c5fd07ca746de831cb0f09904558c22f9ad9b1f
Khaja Hussain <khajakhan0912@gmail.com>
2026-08-11 22:49:52 +0530

    Day 1 - Python basics and input output

---
+++ practiceDay/praticesday1.py
@@ -0,0 +1,15 @@
+#name=(input("enter your name:"))
+#age=int(input ("enter your age:"))
+#college=(input("Enter college name:"))
+#print(name)
+#print(type(age))
+#print(college)
+
+
+#
+a=int(input("enter first number:"))
+b=int(input("enter second number :"))
+print("sum",a+b)
+print("sub",a-b)
+print("mul",a*b)
+print("div",a/b)
